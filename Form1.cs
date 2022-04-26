1f76a4ab3e5dbf2de3f27b4ecd0b6c70cdbec7a7
Mark Angelo Galit <rankm12345@gmail.com>
2022-04-26 22:32:49 +0800

    First Commit

---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace MarkAngeloGalitBSCOE1_5FirstProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mark Angelo Galit", "My Name");
+        }
+    }
+}
